1f3a4393de61cb89bbc2463791786b5d7cca3b4a
Steven <spoulinbishops@gmail.com>
2023-02-12 22:55:09 -0500

    Add files via upload

---
+++ Useful/script_starter.py
@@ -0,0 +1,20 @@
+from subprocess import run
+from time import sleep
+
+# Path and name to the script you are trying to start
+file_path = "test.py" 
+
+restart_timer = 2
+def start_script():
+    try:
+        # Make sure 'python' command is available
+        run("python "+file_path, check=True) 
+    except:
+        # Script crashed, lets restart it!
+        handle_crash()
+
+def handle_crash():
+    sleep(restart_timer)  # Restarts the script after 2 seconds
+    start_script()
+
+start_script()
